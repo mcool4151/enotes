c30b967857b311c15dbed6088653b8e50eb58bb1
apexthepro <avishkadakia1996@gmail.com>
2017-01-08 16:25:39 +0530

    Check

--- javascripts/dropdown-menu.js
+++ javascripts/dropdown-menu.js
@@ -37,17 +37,7 @@ $(".search-icon").click(function(e) {
 
     classname = $(e.target).attr('class').split(' ')[0];
 
-if(classname == 'new'){
-  if(onscreen==0)
-  {
-  $(".new-button-container").append('<ul class="folder-submenu-container" ><li class="create-folder"><i class="create-folder ion-plus icon" ></i><span class="create-folder">Create Folder</span></li><li class="upload"><i class="upload ion-ios-paper icon" ></i><span class="upload">Upload File</span></li></ul>');
-onscreen=1;
-}
-}
-else{
-  $( ".folder-submenu-container" ).remove();
-  onscreen=0;
-}
+
 });
 //$( ".folder-submenu-container" ).remove();
 /*
@@ -59,9 +49,17 @@ $(".dot-icon").click(function(){
 });*/
     $("body").click(function(e) {
  classname = $(e.target).attr('class').split(' ')[0];
-
+ $("h3").text(classname);
     //     alert("classname");
+    if(classname == 'new' && onscreen==0){
 
+      $(".new-button-container").append('<ul class="folder-submenu-container" ><li class="create-folder"><i class="create-folder ion-plus icon" ></i><span class="create-folder">Create Folder</span></li><li class="upload"><i class="upload ion-ios-paper icon" ></i><span class="upload">Upload File</span></li></ul>');
+    onscreen=1;
+    
+    }else if(classname != 'new'){
+      $( ".folder-submenu-container" ).remove();
+      onscreen=0;
+    }
     folderid = $(e.target).parent().attr('id');
     //$("h3").text(folderid);
       if(active!=0){
@@ -80,7 +78,7 @@ $(".dot-icon").click(function(){
         }
         if(active==0)
         {
-          $("#"+folderid).append('<ul class="folder-submenu-container" ><li class="open-with open-with-main"><i class="ion-arrow-expand icon" ></i><span class="open-with">Open with</span><i class="ion-arrow-right-b right" aria-hidden="true"></i><ul class="submenu open-with-sub" ><li><i class="ion-plus icon" ></i><span class="connect-more-apps">connect more apps</span></li></ul></li><li class="move-to"><i class="ion-android-exit icon" ></i><span class="move-to">Move To..</span></li><li class="get-shareable-link"><i class="ion-link icon" ></i><span class="get-shareable-link">Get Shareable Link</span></li><li class="favorite"><i class="ion-ios-star icon" ></i><span class="favorite">Add To Favorite</span></li><li class="rename"><i class="ion-ios-compose icon" ></i><span class="rename">Rename</span></li><li class="details"><i class="ion-ios-information icon" ></i><span class="details">View Details</span></li><li class="download"><i class="ion-archive icon" ></i><span class="download">Download</span></li></ul>');
+          $("#"+folderid).append('<ul class="folder-submenu-container" ><li class="open-with open-with-main"><i class="ion-arrow-expand icon" ></i><span class="open-with">View</span><i class="ion-arrow-right-b right" aria-hidden="true"></i></li><li class="move-to"><i class="ion-android-exit icon" ></i><span class="move-to">Move To..</span></li><li class="get-shareable-link"><i class="ion-link icon" ></i><span class="get-shareable-link">Get Shareable Link</span></li><li class="favorite"><i class="ion-ios-star icon" ></i><span class="favorite">Add To Favorite</span></li><li class="rename"><i class="ion-ios-compose icon" ></i><span class="rename">Rename</span></li><li class="details"><i class="ion-ios-information icon" ></i><span class="details">View Details</span></li><li class="download"><i class="ion-archive icon" ></i><span class="download">Download</span></li></ul>');
           active=1;
           if ( $(window).width() < 480) {
           $(".back-arrow").css({"display": "block"});
