7cc7c9c63ba6e9ce290583bf21d0f09706f43cd8
Kadakia <mcool4151@github.com>
2017-06-04 12:46:46 +0530

    upload java script

--- javascripts/upload.js
+++ javascripts/upload.js
@@ -1,2 +1,10 @@
-alert("entred");
+//alert("entred");
 //$("#file:after").css({ 'border-color':  });
+$(document).ready(function(){
+    $("#file0").click(function(){
+  //    alert("entred2");
+        $("<div class=\"upload-bar\">abc</div>").insertAfter("#file-name0");
+        $("#file0 .file-preview").css({ 'opacity': '0.2' });
+        $(".upload-bar").css({ "color":"red","height":"2px","width":"20%","background":"orange"  });
+    });
+});
